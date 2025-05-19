ee84b5a219b9fb3154e2f27c5178535c5162a55c
Svitlana <sv.kazmirchuk@gmail.com>
2025-05-19 15:10:20 +0300

    Додано налаштування для s3

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -56,8 +56,9 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'visibility' => 'public',
             'throw' => false,
-            'report' => false,
+            'report' => false,            
         ],
 
     ],
