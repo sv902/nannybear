3d83df59597c00794d612b25d4339ed0a429b621
Svitlana <sv.kazmirchuk@gmail.com>
2025-05-20 20:27:02 +0300

    Оновлено контролера профайла

--- backend/app/Http/Controllers/Api/NannyProfileController.php
+++ backend/app/Http/Controllers/Api/NannyProfileController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\NannyProfile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class NannyProfileController extends Controller
 {
