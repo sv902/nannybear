0cd9e6bb7de8a1d4e89dfce6264f48983f312500
Svitlana <sv.kazmirchuk@gmail.com>
2025-05-21 14:36:56 +0300

    Оновлено маршрут тест веб

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
 use App\Http\Middleware\DisableCsrfForApi;
+use Illuminate\Http\Middleware\HandleCors;
 
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -15,7 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-      //  $middleware->statefulApi();
+      // $middleware->statefulApi();
+       $middleware->appendToGroup('api', HandleCors::class);
         
         // Додаємо middleware для ролей
         $middleware->alias([
