14f956a47d7f74aadc6ac5b33d3fd0194c9bed51
Svitlana <sv.kazmirchuk@gmail.com>
2025-05-21 14:18:13 +0300

    Оновлено маршрут тест веб

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -17,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
 
-$middleware->appendToGroup('api', \Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
 
         
         // Додаємо middleware для ролей
